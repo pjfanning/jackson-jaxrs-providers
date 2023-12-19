df91f4c1f0e6b610f90d1058e56789dbe87fd03a
PJ Fanning <pjfanning@users.noreply.github.com>
2023-12-19 16:10:44 +0100

    test with jsr311-compat

--- datatypes/src/main/java/com/fasterxml/jackson/datatype/jaxrs/Jaxrs2TypesModule.java
+++ datatypes/src/main/java/com/fasterxml/jackson/datatype/jaxrs/Jaxrs2TypesModule.java
@@ -1,17 +1,10 @@
 package com.fasterxml.jackson.datatype.jaxrs;
 
-import javax.ws.rs.core.Link;
-
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
- * Simple datatype module that adds serialization and deserialization
- * support for following JAX-RS 2.0 types:
- *<ul>
- * <li>{@link javax.ws.rs.core.Link}: serialized using "link header" representation
- *  </li>
- * </ul>
+ * Simple datatype module that adds serialization and deserialization.
  *
  * @since 2.8
  */
@@ -25,9 +18,6 @@ public class Jaxrs2TypesModule extends SimpleModule
         // 26-Dec-2015, tatu: TODO: add custom serializers/deserializers for Link,
         //    other new JAX-RS 2.0 datatypes.
 
-        addDeserializer(Link.class, new LinkDeserializer());
-
-        addSerializer(Link.class, new ToStringSerializer(Link.class));
     }
 
     // yes, will try to avoid duplicate registrations (if MapperFeature enabled)
